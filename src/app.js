6c58862ecd9dec33bfbcbb2f10fe507d561c5ae3
Julieta Castillo <julietatcastillo04@gmail.com>
2025-12-03 23:07:23 -0300

    Los jugadores ya se une a una misma partida, falta la logica y un problema con el anfitrion

--- src/app.js
+++ src/app.js
@@ -3,22 +3,22 @@ import handlebars from 'express-handlebars'
 import __dirname from "./utils.js";
 import { Server } from "socket.io";
 import viewsRouter from './routes/views.router.js'
-import {Tateti} from './public/js/Tateti.js'
+import { Tateti } from './public/js/Tateti.js'
 
 //Creamos la variable app
 const app = express();
 
 //[LINEAS PRINCIPALES PARA UNA PLANTILLA]
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname + "/public"));
 //Indicamos que vamos usar el motor de handlebars
 app.engine('handlebars', handlebars.engine());
 //Configuramos la ruta en la que se encuentra la vista
-app.set("views", __dirname+"/views");
+app.set("views", __dirname + "/views");
 app.set('view engine', 'handlebars');
 
 app.use('/', viewsRouter);
 //Generamos el servidor que este conectandose en el puerto 8080
-const server = app.listen(8080, ()=>console.log("Escuchando..."));
+const server = app.listen(8080, () => console.log("Escuchando..."));
 const io = new Server(server); //Le indicamos en que puerto debe estar escuchando 
 
 //lista de mensaje
@@ -26,28 +26,48 @@ const message = [];
 const tateti = new Tateti();
 
 //io.on -> Escuchador de evento, si se produce una conexion en el servidor
-io.on('connection', socket =>{
+io.on('connection', socket => {
     //IO es el servidor | SOCKET son los clientes que se conecten
     console.log("socket connected");
     socket.emit('logs', message); //para que los mensajes vayan al cliente que se esta conectando
-    
+
     //Conectamos con la emision que esta haciendo el cliente
-    socket.on('message', data=>{
+    socket.on('message', data => {
         message.push(data); //guardamos el dato en nuestra lista
         console.log(data)
         io.emit('logs', message); //Enviamos los mensajes para que TODOS lo vean
     })
-    socket.on('messageIngreso', data=>{
+    socket.on('messageIngreso', data => {
         console.log(data)
     })
     socket.on('authenticated', username => {
         // Mensaje de bienvenida para el nuevo usuario
-        const welcomeMsg = {config: "bienvenida", message: `${username} se ha unido al chat` };
+        const welcomeMsg = { config: "bienvenida", message: `${username} se ha unido al chat` };
         message.push(welcomeMsg);                  // lo guardamos en el historial
         socket.broadcast.emit('logs', message);    // lo ven TODOS MENOS el que entra
     });
 
     socket.on("tateti:start", username => {
-        
+        if (tateti.isFull()) {
+            socket.emit("tateti:full", "La partida es llena!")
+            return;
+        }
+
+        const ok = tateti.addPlayer(socket.id, username); //Agregamos el jugador
+        if (!ok) {
+            socket.emit("tateti:error", "No se pudo unir al jugador");
+            return;
+        }
+
+        if (tateti.isFull()) {
+            tateti.players.forEach(p => {
+                io.to(p.id).emit("tateti:players", {
+                    players: tateti.players,  
+                    board: tateti.board,
+                    turn: tateti.turn
+                });
+            });
+        }
+
     })
 }) 
--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -1,6 +1,9 @@
 const btnTaTeti = document.getElementById("btnTaTeTi");
 const contendorLoading = document.getElementById("contenedorLoading");
-console.log(btnTaTeti)
+const logsPanel = document.getElementById("logsPanel");
+
+
+
 //Este es el cliente 
 const socket = io({
     //flag de conexion con el servidor
@@ -39,6 +42,42 @@ chatBox.addEventListener('keyup', evento =>{
     }
 })
 
+//Configramos nuestro socket listener del cliente
+socket.on('logs', data => {
+    let message = "";
+    //Como el io nos devuelve un arreglo
+    data.forEach(msg => {
+        if(msg.config === "bienvenida") message+= `<p class="msgBienvenida">${msg.message}</p>`
+        else if(msg.config === "game") {
+            message += `
+                <div class="game">
+                    <img src='${msg.image}' alt="Logo de game" width="70"/>
+                    <div>
+                        <p>${msg.message}</p>
+                        <button type="button" class="btn btn-primary btn-unirse" data-bs-toggle="modal" data-bs-target="#contenedorGame">Unirse</button>
+                    </div>
+                </div>    
+            `
+        }
+        else message += `<p>${msg.user} dice: ${msg.message}</p>`
+    });
+    //lo agregamos al HTML
+    logsPanel.innerHTML = message;
+})
+
+/***********
+ * CONFIGURACIÓN DEL TATETI
+ ***********/
+
+//EVENTOS DE CLICK DENTRO DE LOS LOGS
+logsPanel.addEventListener("click", evento =>{
+    const btn = evento.target.closest(".btn-unirse");
+    console.log(btn);
+    
+    if(!btn) return; //Si no fue el boton de unirse, no hacemos nada
+    socket.emit("tateti:start", user); //Mandamos el datos del jugador
+})
+
 btnTaTeti.addEventListener('click', (evento) =>{
     contendorLoading.innerHTML = `
         <div class="spinner-border text-primary" role="status"></div>
@@ -47,27 +86,24 @@ btnTaTeti.addEventListener('click', (evento) =>{
 
     /*Detectar cuando un jugador ingrese como contrincante. Vaciar el loading y mostrar a ambos usuarios el modal con sus datos. Esto deberia modificarse con los emit y on para poder configurar bien los turnos y la logica del juego*/
     socket.emit("tateti:start", user)
-    socket.emit('message', {config: "game", image:"image/tic-tac-toe.png", message: `${user} a comenzado un juego...`})
+
+    
+    socket.emit('message', {config: "game", image:"image/tic-tac-toe.png", message: `${user} a comenzado un juego...`, player: user});
 })
 
-//Configramos nuestro socket listener del cliente
-socket.on('logs', data => {
-    const logsPanel = document.getElementById("logsPanel");
-    let message = "";
-    //Como el io nos devuelve un arreglo
-    data.forEach(msg => {
-        if(msg.config === "bienvenida") message+= `<p class="msgBienvenida">${msg.message}</p>`
-        else if(msg.config === "game") message += `
-            <div class="game">
-                <img src='${msg.image}' alt="Logo de game" width="70"/>
-                <div>
-                    <p>${msg.message}</p>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#contenedorGame">Unirse</button>
-                </div>
-            </div>    
-        `
-        else message += `<p>${msg.user} dice: ${msg.message}</p>`
-    });
-    //lo agregamos al HTML
-    logsPanel.innerHTML = message;
+socket.on("tateti:players", data =>{
+    const anfitrion = document.getElementById("Anfitrion");
+    const contrincante = document.getElementById("Participante");
+    const modelLoading = new bootstrap.Modal(document.getElementById("loading"));
+
+    console.log(data)
+
+    anfitrion.innerText = data.players[0].username
+    contrincante.innerText = data.players[1].username
+    modelLoading.hide()
+
+    /***********
+     * Debemos cerrar el modal loading al anfitrion, el contricante ya ve el tablero con los datos! 
+     * Falta aplicar la logica
+     */
 })
